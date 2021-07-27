e449d85032fa2dee50362f5d7d85584acecc74e1
Bondok6 <kirolousbondok6@gmail.com>
2021-07-27 21:04:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const section1 = document.querySelector('#section--1');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+// Modal window
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// Button scroll
+btnScrollTo.addEventListener('click', () => {
+  const s1coords = section1.getBoundingClientRect();
+
+  window.scrollTo({
+    left: s1coords.left + pageXOffset,
+    top: s1coords.top + pageYOffset,
+    behavior: 'smooth'
+  });
+})
