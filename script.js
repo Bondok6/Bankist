0a773ab4ae66069dec62fed5f858accd35a5bade
Bondok6 <kirolousbondok6@gmail.com>
2021-07-28 06:53:03 +0200

    tabbed component

--- script.js
+++ script.js
@@ -77,3 +77,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     });
   }
 });
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  // console.log(clicked);
+
+  // Guard Clause
+  if (!clicked) return;
+
+  // Active tabs
+  tabs.forEach((t) => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // Active content area
+  tabsContent.forEach((c) => c.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+  
+})
+
