7f241619a3d967e0cb9df1ebbbfb0e95488f0720
Bondok6 <kirolousbondok6@gmail.com>
2021-07-28 10:17:45 +0200

    sticky navigation

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
 const section1 = document.querySelector('#section--1');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 
 // Modal window
 const openModal = function (e) {
@@ -79,10 +83,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
   // console.log(clicked);
@@ -100,3 +100,26 @@ tabsContainer.addEventListener('click', function (e) {
   
 })
 
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const hoverLink = e.target;
+    const otherLinks = e.target.closest('nav').querySelectorAll('.nav__link');
+    const logo = e.target.closest('nav').querySelector('img');
+
+    otherLinks.forEach((l) => {
+      if (l !== hoverLink) l.style.opacity = this;
+    })
+    logo.style.opacity = this;
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation || Bad performance to us scroll event
+const initCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  if (scrollY > initCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+})
